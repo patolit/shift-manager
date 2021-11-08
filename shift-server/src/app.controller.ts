d5e5ee6f9d9fa1b8617552f7b9ef9068d0506325
patolit <litvinz@gmail.com>
2021-11-08 16:01:44 +0200

    Implementing JWT  from nest guide

--- shift-server/src/app.controller.ts
+++ shift-server/src/app.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from './auth/local-auth.guards';
+import { AuthService } from './auth/auth.service';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private authService: AuthService,
+    private appService: AppService,
+  ) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
 
   @Get()
   getHello(): string {
