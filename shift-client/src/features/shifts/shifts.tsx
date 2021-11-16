ef016f94107a54249c8e6000695fcb9e4314adb1
patolit <litvinz@gmail.com>
2021-11-16 11:49:09 +0200

    created shifts place holder

---
+++ shift-client/src/features/shifts/shifts.tsx
@@ -0,0 +1,16 @@
+function Shifts() {
+  return (
+    <>
+      <h1> Shifts Table </h1>
+      <table>
+        <tr>
+          <td>test</td>
+          <td>test</td>
+          <td>test</td>
+        </tr>
+      </table>
+    </>
+  );
+}
+
+export default Shifts;
